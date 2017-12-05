f9f635ac0e439ef3d5438952a854a4c90996b653
nooldey <openzws@163.com>
2017-12-05 15:09:23 +0800

    renew

--- config/site.js
+++ config/site.js
@@ -2,10 +2,10 @@
  * @Author: nooldey 
  * @Date: 2017-11-07 12:33:28 
  * @Last Modified by: nooldey
- * @Last Modified time: 2017-11-23 17:41:14
+ * @Last Modified time: 2017-12-05 15:08:47
  * @Description: 全站配置信息
  */
-const siteTitle = '前端实验室-工具集'
+const siteTitle = 'vue_gantt_demo'
 const uri = 'http://127.0.0.1:9527'
 
 module.exports = {
